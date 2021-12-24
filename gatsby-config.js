d6088b22b373c111859c0ffc346b1110b575cb56
Jorge García <jgarcia@intersog.com>
2021-12-24 11:35:14 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "My Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
